57e46ebd382804b5a572c7e7e81dca8e18ebace5
crazy_stone <1445830802@qq.com>
2020-02-21 19:31:29 +0800

    删除node_modules.rar

---
+++ src/directive/permission/config.js
@@ -0,0 +1,44 @@
+/* 按钮权限 */
+// 学生管理页面
+export const STUDENT_ADD = ['student:save']
+export const STUDENT_DELETE = ['student:delete']
+export const STUDENT_UPDATE = ['student:update']
+export const STUDENT_GET = ['student:pageList']
+// 教师管理页面
+export const TEACHER_ADD = []
+export const TEACHER_DELETE = []
+export const TEACHER_UPDATE = []
+export const TEACHER_GET = []
+export const ALLOCATE_ROLE = []
+// 角色管理页面
+export const ROLE_ADD = []
+export const ROLE_DELETE = []
+export const ROLE_UPDATE = []
+export const ROLE_GET = []
+export const ALLOCATE_PERMISSION = []
+// 权限管理
+export const PERMISSION_ADD = []
+export const PERMISSION_DELETE = []
+export const PERMISSION_UPDATE = []
+export const PERMISSION_GET = []
+// 课程管理
+export const COURCE_ADD = []
+export const COURCE_DELETE = []
+export const COURCE_UPDATE = []
+export const COURCE_GET = []
+// 教学任务管理
+export const TEACH_TASK_ADD = []
+export const TEACH_TASK_DELETE = []
+export const TEACH_TASK_UPDATE = []
+export const TEACH_TASK_GET = []
+// 日志管理
+export const LOG_GET = []
+export const LOG_DOWNLOAD = []
+
+export default {
+  STUDENT_ADD,
+  STUDENT_DELETE,
+  STUDENT_UPDATE,
+  STUDENT_GET
+}
+
---
+++ src/directive/permission/index.js
@@ -0,0 +1,21 @@
+import store from '@/store'
+
+export default {
+  inserted(el, binding, vnode) {
+    const { value } = binding
+    const permissions = store.getters && store.getters.permission
+    const permissionSources = permissions.map(item => item.permissionSource)
+    console.log('permissionSources', permissionSources)
+    if (value && value instanceof Array && value.length > 0) {
+      const needPermissionSources = value
+      const hasPermission = needPermissionSources.every(item => {
+        permissionSources.include(item)
+      })
+      if (!hasPermission) {
+        el.parentNode && el.parentNode.removeChild(el)
+      }
+    } else {
+      throw new Error(`need permissions! Like v-permission="[STUDENT_ADD]"`)
+    }
+  }
+}
