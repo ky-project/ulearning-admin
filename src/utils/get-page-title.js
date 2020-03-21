395336e40b81c3d4f4833c936c382b0b6504c8cc
luyuhao <ludaye1112@163.com>
2020-03-22 02:35:13 +0800

    描述：修改title和启动多次加载页面问题 日期：2020-03-22

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,10 +1,10 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+const title = defaultSettings.title || 'U-Learning后台管理系统'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
-    return `${pageTitle} - ${title}`
+    return `${title} - ${pageTitle}`
   }
   return `${title}`
 }
